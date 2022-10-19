35ef3fdaffebdc0209f24ea680963013aa2923d7
Gamze Erdogan <ckrgamze@gmail.com>
2022-10-19 10:54:22 +0200

    mock service changement 2

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,93 +1,112 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import CaloryProteinGlucideLipide from '../components/CaloryProteinGlucideLipide';
-import { useData } from '../data/getData';
-import caloriesIcon from '../images/caloriesIcon.png';
-import protein from '../images/protein.png'
-import apple from '../images/apple.png'
-import cheeseburger from '../images/cheeseburger.png'
-import styled from 'styled-components';
-import AddBarChart from '../components/Activity/AddBarChart';
-import AddLineChart from '../components/DurationSession/AddLineChart';
-import AddRadarChart from '../components/AddRadarChart';
-import AddPieChart from '../components/AddPieChart';
-import ErrorPage from './ErrorPage';
+import React from "react";
+import { useParams } from "react-router-dom";
+import CaloryProteinGlucideLipide from "../components/CaloryProteinGlucideLipide";
+import { useData } from "../data/getData";
+import caloriesIcon from "../images/caloriesIcon.png";
+import protein from "../images/protein.png";
+import apple from "../images/apple.png";
+import cheeseburger from "../images/cheeseburger.png";
+import styled from "styled-components";
+import AddBarChart from "../components/Activity/AddBarChart";
+import AddLineChart from "../components/DurationSession/AddLineChart";
+import AddRadarChart from "../components/AddRadarChart";
+import AddPieChart from "../components/AddPieChart";
+import ErrorPage from "./ErrorPage";
 
 const StyleMain = styled.main`
-    margin-left:150px;
-    margin-top:50px;
-`
+    margin-left: 150px;
+    margin-top: 50px;
+`;
 const StyleHeaderDiv = styled.div`
-    margin-left:30px;
-`
+    margin-left: 30px;
+`;
 const StyleContainerMain = styled.main`
-    display:flex;
-    gap:80px;
-`
+    display: flex;
+    gap: 80px;
+`;
 const StyleAside = styled.aside`
-    display:flex;
-    flex-direction:column;
-    height:600px;
-    justify-content:space-between;
-    align-items:flex-end;
-    margin-right:40px;
-`
+    display: flex;
+    flex-direction: column;
+    height: 600px;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin-right: 40px;
+`;
 const StyleNameSpan = styled.span`
-    color:#ff0101;
-    font-size:49px;
-`
+    color: #ff0101;
+    font-size: 49px;
+`;
 const StyleBonjourP = styled.p`
-    color:black;
-    font-size:48px;
-    margin-bottom:0;
-`
+    color: black;
+    font-size: 48px;
+    margin-bottom: 0;
+`;
 const StyleChartContainerDiv = styled.div`
-   width:750px;
-   height:350px;
-   display:flex;
-   flex-direction:column;
-   justify-content:space-between;
-   align-items:center;
-   gap:50px;
-   
-`
+    width: 750px;
+    height: 350px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    gap: 50px;
+`;
 const StyleDownChartContainerDiv = styled.div`
-    display:flex;
-    width:750px;
-    gap:10px;
-    justify-content:space-between;
-`
-
+    display: flex;
+    width: 750px;
+    gap: 10px;
+    justify-content: space-between;
+`;
 
 const Dashboard = () => {
-    const {id : userId}= useParams();
-    const  getUser  = useData(userId);
-    if (!getUser.userName) return <ErrorPage  />
-  return (
-    <StyleMain>
-        <StyleHeaderDiv>
-          <StyleBonjourP>Bonjour, <StyleNameSpan>{ getUser.userName}</StyleNameSpan></StyleBonjourP>
-          <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-        </StyleHeaderDiv>
-        <StyleContainerMain>
-            <StyleChartContainerDiv>
-                <AddBarChart userID={userId}/>
-                <StyleDownChartContainerDiv>
-                    <AddLineChart userID={userId}/>
-                    <AddRadarChart userID={userId} />
-                    <AddPieChart dataOfPieChart={getUser.todayScore}/>
-                </StyleDownChartContainerDiv>
-            </StyleChartContainerDiv> 
-            <StyleAside>
-                <CaloryProteinGlucideLipide  CalorieSource={ getUser.keyDataCalorie/1000} IconSource={caloriesIcon} title={'Calories'} unit={'kCal'}/>
-                <CaloryProteinGlucideLipide  CalorieSource={ getUser.keyDataProtein} IconSource={protein} title={'Protoines'} unit={'g'}/>
-                <CaloryProteinGlucideLipide  CalorieSource={ getUser.keyDataCarbHid} IconSource={apple} title={'Glucides'} unit={'g'}/>
-                <CaloryProteinGlucideLipide  CalorieSource={ getUser.keyDataLipid} IconSource={cheeseburger} title={'Lipides'} unit={'g'}/>
-            </StyleAside>
-        </StyleContainerMain>
-    </StyleMain>
+    const { id: userId } = useParams();
+    const getUser = useData(userId);
+    if (!getUser.userName) return <ErrorPage />;
+    return (
+        <StyleMain>
+            <StyleHeaderDiv>
+                <StyleBonjourP>
+                    Bonjour, <StyleNameSpan>{getUser.userName}</StyleNameSpan>
+                </StyleBonjourP>
+                <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+            </StyleHeaderDiv>
+            <StyleContainerMain>
+                <StyleChartContainerDiv>
+                    <AddBarChart userID={userId} />
+                    <StyleDownChartContainerDiv>
+                        <AddLineChart userID={userId} />
+                        <AddRadarChart userID={userId} />
+                        <AddPieChart dataOfPieChart={getUser.todayScore} />
+                    </StyleDownChartContainerDiv>
+                </StyleChartContainerDiv>
+                <StyleAside>
+                    <CaloryProteinGlucideLipide
+                        CalorieSource={getUser.keyDataCalorie / 1000}
+                        IconSource={caloriesIcon}
+                        title={"Calories"}
+                        unit={"kCal"}
+                    />
+                    <CaloryProteinGlucideLipide
+                        CalorieSource={getUser.keyDataProtein}
+                        IconSource={protein}
+                        title={"Protoines"}
+                        unit={"g"}
+                    />
+                    <CaloryProteinGlucideLipide
+                        CalorieSource={getUser.keyDataCarbHid}
+                        IconSource={apple}
+                        title={"Glucides"}
+                        unit={"g"}
+                    />
+                    <CaloryProteinGlucideLipide
+                        CalorieSource={getUser.keyDataLipid}
+                        IconSource={cheeseburger}
+                        title={"Lipides"}
+                        unit={"g"}
+                    />
+                </StyleAside>
+            </StyleContainerMain>
+        </StyleMain>
+    );
+};
 
-  )
-}
-
-export default Dashboard
+export default Dashboard;
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 /**
  * Render a classic 404 page.
@@ -7,38 +7,38 @@ import { Link } from 'react-router-dom'
  * @constructor
  */
 function ErrorPage() {
-  return (
-    <Container>
-      <h1>Oupppss!</h1>
-      <h1>404</h1>
-      <p>La user ou la page que vous recherchez n'existe pas</p>
-      <Link to="/">Retourner à la page d'accueil</Link>
-    </Container>
-  )
+    return (
+        <Container>
+            <h1>Oupppss!</h1>
+            <h1>404</h1>
+            <p>La user ou la page que vous recherchez n'existe pas</p>
+            <Link to="/">Retourner à la page d'accueil</Link>
+        </Container>
+    );
 }
 
-export default ErrorPage
+export default ErrorPage;
 
 const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  h1 {
-    font-size: 6rem;
-    color: red;
-  }
-  p {
-    font-size: 1.5rem;
-    margin: 2rem;
-  }
-  a,
-  a:visited {
-    color: dodgerblue;
-    font-size: 1.25rem;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
-`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+        font-size: 6rem;
+        color: red;
+    }
+    p {
+        font-size: 1.5rem;
+        margin: 2rem;
+    }
+    a,
+    a:visited {
+        color: dodgerblue;
+        font-size: 1.25rem;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+`;
